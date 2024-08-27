b88d62c90a07d2dd0dabc8bcaab3174f1d255877
Jefferson Roberto <roberto.jefferson98@gmail.com>
2024-08-26 23:05:18 -0300

    Update 44_for_in.py

--- 44_for_in.py
+++ 44_for_in.py
@@ -1,9 +1,19 @@
-texto = 'Python'
+# texto = 'Python'
 
-i = 0
+# i = 0
 
-tamanho_string = len(texto)
+# tamanho_string = len(texto)
 
-while i < tamanho_string:
-    print(texto[i])
-    i += 1
+# while i < tamanho_string:
+#     print(texto[i], i)
+#     i += 1
+
+senha_salva = '123456'
+senha_digitada = ''
+repeticoes = 0
+
+while senha_salva != senha_digitada:
+    senha_digitada = input(f'Sua senha ({repeticoes}x)')
+    repeticoes += 1
+
+print('Aquele laço acima pode ter repetições infinitas')
