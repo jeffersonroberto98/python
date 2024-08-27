24db2eb28030d3fc155a62b8c1aaa517b891836c
Jefferson Roberto <roberto.jefferson98@gmail.com>
2024-08-27 18:47:02 -0300

    ex 44 e ex 45

--- 44_for_in.py
+++ 44_for_in.py
@@ -8,12 +8,21 @@
 #     print(texto[i], i)
 #     i += 1
 
-senha_salva = '123456'
-senha_digitada = ''
-repeticoes = 0
+# senha_salva = '123456'
+# senha_digitada = ''
+# repeticoes = 0
 
-while senha_salva != senha_digitada:
-    senha_digitada = input(f'Sua senha ({repeticoes}x)')
-    repeticoes += 1
+# while senha_salva != senha_digitada:
+#     senha_digitada = input(f'Sua senha ({repeticoes}x):')
+#     repeticoes += 1
 
-print('Aquele laço acima pode ter repetições infinitas')
+# print(repeticoes)
+# print('Aquele laço acima pode ter repetições infinitas')
+
+texto = 'python'
+novo_texto = ''
+
+for letra in texto:
+    novo_texto += f'*{letra}'
+    print(letra)
+print(novo_texto + '*')
