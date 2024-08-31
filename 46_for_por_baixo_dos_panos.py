d5a25023ecf89630954a92a9b0aadbcbae25465c
Jefferson Roberto <roberto.jefferson98@gmail.com>
2024-08-31 16:32:28 -0300

    Create 46_for_por_baixo_dos_panos.py

---
+++ 46_for_por_baixo_dos_panos.py
@@ -0,0 +1,9 @@
+'''
+Iterável -> str, range, etc (__iter__)
+Iterador -> quem sabe entregar um valor por vez
+next -> me entregue o próximo valor
+iter -> me entregue seu iterador
+'''
+
+texto = iter('Jefferson') # __iter__()
+print(texto)
